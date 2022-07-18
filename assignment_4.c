6f0259a023d9f3a9b161ca81317a5ca79a70643b
sanjayypall <109548942+sanjayypall@users.noreply.github.com>
2022-07-19 00:27:09 +0530

    Add files via upload

---
+++ assignment_4.c
@@ -0,0 +1,101 @@
+// #include<stdio.h>
+// int main(){
+//     for (int i = 1; i < 6; i++)
+//     {
+//         printf("MySirG\n");
+//     }
+//     return 0;
+// }
+
+// write a program to print first 10 natural number
+#include<stdio.h>
+// int main(){
+//     int a=1;
+//     do
+//     {
+//         printf("%d ",a);
+//         a++;
+//     } while (a<11);
+//     return 0;
+// }
+
+// int main(){
+//     int a=10;
+//     do
+//     {
+//         printf("%d ",a);
+//         a--;
+//     } while (a>0);
+//     return 0;
+// }
+
+// int main(){
+//     int a=1;
+//     do
+//     {
+//         printf("%d ",a);
+//         a=a+2;
+//     } while (a<20);
+//     return 0;
+// }
+
+// int main(){
+//     int a=19;
+//     do
+//     {
+//         printf("%d ",a);
+//         a=a-2;
+//     } while (a>0);
+//     return 0;
+// }
+
+// int main(){
+//     int a=2;
+//     do
+//     {
+//         printf("%d ",a);
+//         a=a+2;
+//     } while (a<=20);
+//     return 0;
+// }
+
+
+// int main(){
+//     int a=20;
+//     do
+//     {
+//         printf("%d ",a);
+//         a=a-2;
+//     } while (a>0);
+//     return 0;
+// }
+
+// int main(){
+//     for (int i = 1; i < 11; i++)
+//     {
+//         printf("%d ",i*i);
+//     }
+    
+//     return 0;
+// }
+
+
+
+// int main(){
+//     for (int i = 1; i < 11; i++)
+//     {
+//         printf("%d ",i*i*i);
+//     }
+    
+//     return 0;
+// }
+
+
+int main(){
+    int a=5;
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d*%d=%d\n",a,i,a*i);
+    }
+    return 0;
+}
